7487faf20abd8b212f66d0f43e8c23ce35fd2af1
Benjamin Samuels <benjamin.samuels@belkin.com>
2017-01-30 16:18:08 -0800

    adding text log parameters to only display sent/recvd strings when a test case fails

--- boofuzz/fuzz_logger_text.py
+++ boofuzz/fuzz_logger_text.py
@@ -48,16 +48,26 @@ class FuzzLoggerText(ifuzz_logger_backend.IFuzzLoggerBackend):
     DEFAULT_TEST_CASE_ID = "DefaultTestCase"
     INDENT_SIZE = 2
 
-    def __init__(self, file_handle=sys.stdout, bytes_to_str=DEFAULT_HEX_TO_STR):
+    def __init__(self, file_handle=sys.stdout, bytes_to_str=DEFAULT_HEX_TO_STR, hide_sent_bytes=False, hide_recvd_bytes=False):
         """
         @type file_handle: io.FileIO
         @param file_handle: Open file handle for logging. Defaults to sys.stdout.
 
         @type bytes_to_str: function
         @param bytes_to_str: Function that converts sent/received bytes data to string for logging.
+
+        @type hide_sent_bytes: boolean
+        @param hide_sent_bytes: Only print the bytes sent to target when a test case fails.
+
+        @type hide_recvd_bytes: boolean
+        @param hide_recvd_bytes: Only print the bytes received from the target when a test case fails.
         """
         self._file_handle = file_handle
         self._format_raw_bytes = bytes_to_str
+        self._hide_recvd_bytes = hide_recvd_bytes
+        self._hide_sent_bytes = hide_sent_bytes
+        if self._hide_sent_bytes:
+            self.LOG_SEND_FORMAT = "Transmitted {0} bytes: {1}"
 
     def open_test_step(self, description):
         self._print_log_msg(self.TEST_STEP_FORMAT.format(description),
@@ -72,13 +82,21 @@ class FuzzLoggerText(ifuzz_logger_backend.IFuzzLoggerBackend):
                             indent_level=2)
 
     def log_recv(self, data):
-        self._print_log_msg(self.LOG_RECV_FORMAT.format(self._format_raw_bytes(data)),
-                            indent_level=2)
+        if not self._hide_recvd_bytes:
+            self._print_log_msg(self.LOG_RECV_FORMAT.format(self._format_raw_bytes(data)),
+                                indent_level=2)
+        else:
+            self._last_recvd_string = self.LOG_RECV_FORMAT.format(self._format_raw_bytes(data))
+            
 
     def log_send(self, data):
-        self._print_log_msg(
-            self.LOG_SEND_FORMAT.format(len(data), self._format_raw_bytes(data)),
-            indent_level=2)
+        if not self._hide_sent_bytes:
+            self._print_log_msg(
+                self.LOG_SEND_FORMAT.format(len(data), self._format_raw_bytes(data)),
+                indent_level=2)
+        else:
+            self._last_sent_string = self.LOG_SEND_FORMAT.format(len(data), self._format_raw_bytes(data))
+            
 
     def log_info(self, description):
         self._print_log_msg(self.LOG_INFO_FORMAT.format(description),
@@ -90,6 +108,14 @@ class FuzzLoggerText(ifuzz_logger_backend.IFuzzLoggerBackend):
 
     def log_fail(self, description=""):
         self._print_log_msg(self.LOG_FAIL_FORMAT.format(description),
+                            indent_level=3)
+
+        if self._hide_sent_bytes and self._last_sent_string is not None:
+            self._print_log_msg(self._last_sent_string,
+                            indent_level=3)
+            
+        if self._hide_recvd_bytes and self._last_recvd_string is not None:
+            self._print_log_msg(self._last_recvd_string,
                             indent_level=3)
 
     def log_pass(self, description=""):
